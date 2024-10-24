c6d19e0d4eb26f9c1e6b148507a04c88befe9570
RomaYushchenko <yushenkoromaf7@gmail.com>
2024-10-24 16:36:58 +0300

    Initial commit

---
+++ show_scout_service/src/main/java/com/ua/yushchenko/ShowScoutServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ua.yushchenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowScoutServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowScoutServiceApplication.class, args);
+    }
+}
